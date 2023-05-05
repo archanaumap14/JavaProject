b655c79451154fe35d611d52b3e333832859c232
archanaumap14 <archanaumap14@gmail.com>
2023-05-05 13:59:59 +0530

    first commit: code

---
+++ src/com/velocity/demo/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		ArrayList<String>list=new ArrayList<String>();
+		list.add("Shyam");
+		list.add("Mohan");
+		list.add("Vinod");
+		
+		System.out.println(list);
+
+	}
+
+}
---
+++ src/com/velocity/demo/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.velocity.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		LinkedList<Integer>list=new LinkedList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		System.out.println(list);
+
+	}
+
+}
